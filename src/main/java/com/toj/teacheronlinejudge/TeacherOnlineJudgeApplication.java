787a1f5cbd23c477aa39c49ce9d2e20cfe72c39a
leekyukin <leekujin14@gmail.com>
2022-08-25 16:51:50 +0900

    SET :: initial

---
+++ src/main/java/com/toj/teacheronlinejudge/TeacherOnlineJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.toj.teacheronlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherOnlineJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherOnlineJudgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toj/teacheronlinejudge/TeacherOnlineJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toj.teacheronlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherOnlineJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
