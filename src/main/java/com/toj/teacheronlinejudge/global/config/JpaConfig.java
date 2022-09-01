05cdea4160df9768fd53c6a9d9c16bc77d512de8
leekyukin <leekujin14@gmail.com>
2022-09-01 13:31:12 +0900

    ADD :: BaseTImeEntity

---
+++ src/main/java/com/toj/teacheronlinejudge/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.toj.teacheronlinejudge.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
