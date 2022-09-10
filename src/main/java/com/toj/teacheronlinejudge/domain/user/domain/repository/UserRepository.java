048bc2d4a31878bdf9cd5d43fdf40f2e1d26fb6a
leekyukin <leekujin14@gmail.com>
2022-09-10 13:04:21 +0900

    ADD :: User domain

---
+++ src/main/java/com/toj/teacheronlinejudge/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.toj.teacheronlinejudge.domain.user.domain.repository;
+
+import com.toj.teacheronlinejudge.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
