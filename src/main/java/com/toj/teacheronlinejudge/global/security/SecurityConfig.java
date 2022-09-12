f3daaf2ee7b07694f8a18e3699f9bc5aceac6446
leekyukin <leekujin14@gmail.com>
2022-09-12 23:50:17 +0900

    SET :: Security

---
+++ src/main/java/com/toj/teacheronlinejudge/global/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.toj.teacheronlinejudge.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+
+                .authorizeRequests()
+                .antMatchers("**").permitAll()
+
+                ;
+    }
+}
