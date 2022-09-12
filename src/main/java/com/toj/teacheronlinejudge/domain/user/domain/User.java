66e262860edb003207b210911a903736e98f3f93
leekyukin <leekujin14@gmail.com>
2022-09-12 23:48:45 +0900

    FIX :: 컬럼 오류수정

--- src/main/java/com/toj/teacheronlinejudge/domain/user/domain/User.java
+++ src/main/java/com/toj/teacheronlinejudge/domain/user/domain/User.java
@@ -3,7 +3,10 @@ package com.toj.teacheronlinejudge.domain.user.domain;
 import com.toj.teacheronlinejudge.domain.teacher.domain.Comment;
 import com.toj.teacheronlinejudge.domain.user.domain.type.Authority;
 import com.toj.teacheronlinejudge.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,11 +15,21 @@ import java.util.List;
 @Entity
 @Getter
 @Table(name = "user_tbl")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", nullable = false, unique = false)
+    @Column(name = "user_id", nullable = false, unique = true)
     private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickName", nullable = false, unique = true)
+    private String nickName;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -33,4 +46,15 @@ public class User extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Comment> commentList = new ArrayList<>();
+
+    @Builder
+    public User(String email, String password, String nickName, String name, int grade, int classRoom, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.nickName = nickName;
+        this.name = name;
+        this.grade = grade;
+        this.classRoom = classRoom;
+        this.authority = authority;
+    }
 }
