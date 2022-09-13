0294988214c5e04033689d4e62c982820aaf9b73
leekyukin <leekujin14@gmail.com>
2022-09-13 20:21:35 +0900

    ADD :: Security Authentication

--- src/main/java/com/toj/teacheronlinejudge/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/toj/teacheronlinejudge/global/security/jwt/JwtTokenProvider.java
@@ -1,12 +1,16 @@
 package com.toj.teacheronlinejudge.global.security.jwt;
 
+import com.toj.teacheronlinejudge.global.security.jwt.exception.ExpiredTokenException;
+import com.toj.teacheronlinejudge.global.security.jwt.exception.InvalidTokenException;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
@@ -43,6 +47,29 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public Claims extractAllClaims(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey(SECRET_KEY))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
 
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(JwtProperties.JWT_HEADER);
+        return parseToken(bearer);
+    }
 
+    private String parseToken(String bearerToken) {
+        if (bearerToken !=  null && bearerToken.startsWith(JwtProperties.JWT_PREFIX)) {
+            return bearerToken.replace(JwtProperties.JWT_PREFIX, "");
+        }
+        return null;
+    }
 }
