0fa982864ed297a52beee25fc5f583e9bd34e561
leekyukin <leekujin14@gmail.com>
2022-09-13 14:06:47 +0900

    ADD :: password encode 하기

--- src/main/java/com/toj/teacheronlinejudge/global/security/SecurityConfig.java
+++ src/main/java/com/toj/teacheronlinejudge/global/security/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.toj.teacheronlinejudge.global.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
