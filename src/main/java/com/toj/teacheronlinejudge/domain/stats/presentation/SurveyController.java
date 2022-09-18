cc6721d008f8d24a328d4916368a6ce532125326
leekyukin <leekujin14@gmail.com>
2022-09-18 15:19:29 +0900

    FIX :: url

--- src/main/java/com/toj/teacheronlinejudge/domain/stats/presentation/SurveyController.java
+++ src/main/java/com/toj/teacheronlinejudge/domain/stats/presentation/SurveyController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/teacher/survey/{id}")
+@RequestMapping("/stats/survey/{id}")
 @RequiredArgsConstructor
 public class SurveyController {
 
