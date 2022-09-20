2b5da169bbf89808e139987554c6dcbecc6a6e24
leekyukin <leekujin14@gmail.com>
2022-09-20 21:30:54 +0900

    ADD :: CORS Web Config

---
+++ src/main/java/com/toj/teacheronlinejudge/global/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.toj.teacheronlinejudge.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+        ;
+    }
+}
