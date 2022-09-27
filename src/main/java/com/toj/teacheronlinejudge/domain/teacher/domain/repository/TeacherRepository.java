c81cb51c0434f33d12f3b93d033039df6c680a6c
leekyukin <leekujin14@gmail.com>
2022-09-27 12:28:42 +0900

    FIX :: 선생님 좋아요 순으로 조회하기

--- src/main/java/com/toj/teacheronlinejudge/domain/teacher/domain/repository/TeacherRepository.java
+++ src/main/java/com/toj/teacheronlinejudge/domain/teacher/domain/repository/TeacherRepository.java
@@ -3,8 +3,11 @@ package com.toj.teacheronlinejudge.domain.teacher.domain.repository;
 import com.toj.teacheronlinejudge.domain.teacher.domain.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     List<Teacher> findAllByOrderByTierDesc();
+
+    List<Teacher> findAllByOrderByLikesDesc();
 }
