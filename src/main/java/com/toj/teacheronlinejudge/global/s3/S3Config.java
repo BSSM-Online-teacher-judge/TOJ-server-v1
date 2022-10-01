b60d22daa1b5a50369131226895097046c97ed35
leekyukin <leekujin14@gmail.com>
2022-10-01 14:02:55 +0900

    SET :: S3

---
+++ src/main/java/com/toj/teacheronlinejudge/global/s3/S3Config.java
@@ -0,0 +1,33 @@
+package com.toj.teacheronlinejudge.global.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
---
+++ src/main/java/com/toj/teacheronlinejudge/global/s3/S3Properties.java
@@ -0,0 +1,7 @@
+package com.toj.teacheronlinejudge.global.s3;
+
+public class S3Properties {
+
+    public static final String USER_PROFILE = "user-profile/";
+    public static final String TEACHER_PROFILE = "teacher-profile/";
+}
