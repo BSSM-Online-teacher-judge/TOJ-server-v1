c0ac2853c1844a7003eac20c1dd1a8df68dd0f7e
leekyukin <leekujin14@gmail.com>
2022-10-24 10:24:14 +0900

    FIX :: 유저 정보에 권한 추가

--- src/main/java/com/toj/teacheronlinejudge/domain/user/presentation/dto/response/UserResponseDto.java
+++ src/main/java/com/toj/teacheronlinejudge/domain/user/presentation/dto/response/UserResponseDto.java
@@ -1,6 +1,7 @@
 package com.toj.teacheronlinejudge.domain.user.presentation.dto.response;
 
 import com.toj.teacheronlinejudge.domain.user.domain.User;
+import com.toj.teacheronlinejudge.domain.user.domain.type.Authority;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,15 +15,17 @@ public class UserResponseDto {
     private String nickName;
     private int grade;
     private int classRoom;
+    private Authority authority;
 
     public static UserResponseDto of(User user) {
         return UserResponseDto.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .name(user.getName())
-                .nickName(user.getNickName())
-                .grade(user.getGrade())
-                .classRoom(user.getClassRoom())
-                .build();
+            .id(user.getId())
+            .email(user.getEmail())
+            .name(user.getName())
+            .nickName(user.getNickName())
+            .grade(user.getGrade())
+            .classRoom(user.getClassRoom())
+            .authority(user.getAuthority())
+            .build();
     }
 }
